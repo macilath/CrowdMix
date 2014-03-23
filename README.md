6073c5a837fee34a5931a1d98a85f781cbaa82de
Stephen Ten Eyck <macilath@gmail.com>
2014-03-23 17:24:23 -0500

    Finished up audio_join.py for now, setup.py now prompts for dependency installation, set up dir. structure

--- README.md
+++ README.md
@@ -6,7 +6,8 @@
 Our project is an experimentation into the musical knowledge and preferences of a crowd of workers. This project, given a set of musical tracks as input, will rely on turkers to produce a new song by piecing together snippets from the input tracks.
 
 
+Built on Python 2.7.6
 This project requires the following libraries:
-+ boto
++ [boto](https://github.com/boto/boto)
 + [pydub](https://github.com/jiaaro/pydub/)
 + ffmpeg & libavcodec-extra-53 (available via apt-get)
--- audio/audio_join.py
+++ audio/audio_join.py
@@ -6,7 +6,6 @@ from pydub import AudioSegment
 # Except for the first time, of course
 
 # Read the input files from the command line
-# TODO: Option for crossfading (default=true)
 parser = argparse.ArgumentParser(description="This script joins two MP3 Audio Files, and finalizes them if length is greater than 5 minutes.")
 parser.add_argument('-p', '--primary', help="Primary File Name", required=True)
 parser.add_argument('-a', '--addon', help="Add-on File Name", required=True)
@@ -15,7 +14,6 @@ args = parser.parse_args()
 primary_file = args.primary
 add_on_file = args.addon
 
-# TODO: Error checking - see if bits/ is included in filename or not..
 # Load audio files
 base_audio = AudioSegment.from_mp3(primary_file)
 add_on_audio = AudioSegment.from_mp3(add_on_file)
@@ -26,6 +24,9 @@ result_song = base_audio.append(add_on_audio, crossfade=2000)
 # Determine length of song, if > 5 minutes, add to final folder
 song_length = round(1000 * (result_song.frame_count() / result_song.frame_rate))
 if song_length > 300000:
+	print ("Finishing song...")
 	result_song.export("final/final_song.mp3", format="mp3")
+	print ("Final song located in final/ directory")
 else:
 	result_song.export("bits/building.mp3", format="mp3")
+	print ("Not finished yet. Keep adding on.")
