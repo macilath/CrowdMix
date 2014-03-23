cb756b814ae5cccd6e240bcef187736f4c30ec2b
Stephen Ten Eyck <macilath@gmail.com>
2014-03-23 15:32:23 -0500

    Created audio splitter using pydub library. Split songs into 4 second segments

---
+++ audio/audio_split.py
@@ -0,0 +1,28 @@
+from pydub import AudioSegment
+
+print "Loading audio files..."
+mars = AudioSegment.from_mp3("original/gustav_holst_the_planets_mars.mp3")
+jupiter = AudioSegment.from_mp3("original/gustav_holst_the_planets_jupiter.mp3")
+#saturn = AudioSegment.from_mp3("original/gustav_holst_the_planets_saturn.mp3")
+#uranus = AudioSegment.from_mp3("original/gustav_holst_the_planets_uranus.mp3")
+
+def slice_into_bits(song, song_name_str):
+	print ("Splitting " + song_name_str + ", this may take some time...") 
+	last_4_seconds = song[4000:]
+	first_4_seconds = song[:4000]
+	song_length = round(1000 * (mars.frame_count() / mars.frame_rate))
+	
+	#remember, milliseconds
+	for i in range(4000, int(song_length), 4000):
+		new_bit = song[i:i+4000]
+		new_bit_name = "bits/" + song_name_str + str(i) + "-" + str(i+4000) + ".mp3"
+		new_bit.export(new_bit_name, format="mp3")
+	 
+	last_bit_name = "bits/" + song_name_str + str(song_length - 4000) + "-" + str(song_length) + ".mp3"
+	first_bit_name = "bits/" + song_name_str + str(0) + "-" + str(4000) + ".mp3"	
+	last_4_seconds.export(last_bit_name, format="mp3")
+	first_4_seconds.export(first_bit_name, format="mp3")
+	return
+
+slice_into_bits(mars, "mars")
+slice_into_bits(jupiter, "jupiter")
---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+# Sets up the project
+
+# Install dependencies
+# ffmpeg & libavcodec-extra-53(apt-get)
+# boto (pip)
+# pydub (pip)
+
+# Add AMT credentials
+
+# Add website hostname
+
+# Call to audio splitter
+# Call to Turk poster
+# Verification stuff, posting of bits on webpage, etc.
